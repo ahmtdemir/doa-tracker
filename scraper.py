f2fa74f9333066d95cd821ddb993519ea9ddc3a8
habercitr <31370379+ahmtdemir@users.noreply.github.com>
2026-07-18 10:55:13 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,37 +1,41 @@
 import requests
-
-URL = "https://dbysmgw.doa.gov.tr/dbys/v3/web/rvm/search?pageNumber=1&pageSize=100"
-
-payload = {
-    "lat": 37.21596145629883,
-    "lon": 28.36799430847168,
-    "distance": 2466,
-    "userLat": 37.212301266949034,
-    "userLon": 28.354810181393365
-}
+from config import API_URL, PAYLOAD, TAKIP_MAKINELERI
+from telegram import telegram_gonder
 
 
 def siteyi_test_et():
-    response = requests.post(URL, json=payload)
+
+    response = requests.post(API_URL, json=PAYLOAD, timeout=20)
+
+    if response.status_code != 200:
+        print("API Hatası:", response.status_code)
+        return
 
     data = response.json()
 
-    print("=" * 50)
-    print("Makine Sayısı:", len(data["rvmList"]))
-    print("=" * 50)
+    bulundu = False
 
     for makina in data["rvmList"]:
 
-        print()
-        print(makina["definition"]["name"])
+        isim = makina["definition"]["name"]
+
+        if not any(takip in isim for takip in TAKIP_MAKINELERI):
+            continue
+
+        bulundu = True
+
+        mesaj = f"📍 {isim}\n\n"
 
         for kutu in makina["binList"]:
 
-            durum = "UYGUN" if kutu["state"] else "DOLU"
+            durum = "✅ UYGUN" if kutu["state"] else "❌ DOLU"
 
-            print(
-                kutu["contentType"],
-                kutu["level"],
-                "%",
-                durum
+            mesaj += (
+                f"{kutu['contentType'].upper()} : "
+                f"{kutu['level']}%  {durum}\n"
             )
+
+        telegram_gonder(mesaj)
+
+    if not bulundu:
+        print("Takip edilen makine bulunamadı.")
