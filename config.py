3b7380ff132457d52cf48ba2a837c13dff064e0a
habercitr <31370379+ahmtdemir@users.noreply.github.com>
2026-07-18 11:06:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,24 @@ CHAT_ID = "868647307"
 
 API_URL = "https://dbysmgw.doa.gov.tr/dbys/v3/web/rvm/search?pageNumber=1&pageSize=100"
 
-PAYLOAD = {
-    "lat": 37.21596145629883,
-    "lon": 28.36799430847168,
-    "distance": 2466,
-    "userLat": 37.212301266949034,
-    "userLon": 28.354810181393365
-}
+SEARCH_POINTS = [
+    {
+        "name": "Milas",
+        "lat": 37.21596145629883,
+        "lon": 28.36799430847168,
+        "distance": 10000,
+        "userLat": 37.212301266949034,
+        "userLon": 28.354810181393365,
+    },
+    {
+        "name": "Menteşe",
+        "lat": 37.19468688964844,
+        "lon": 28.38709831237793,
+        "distance": 10000,
+        "userLat": 37.212301266949034,
+        "userLon": 28.354810181393365,
+    }
+]
 
 TAKIP_MAKINELERI = [
     "Q681 BİM NERGİS / MENTEŞE - MUĞLA",
