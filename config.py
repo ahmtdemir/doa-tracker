963dc35b83a1663bf9be57cbeeb355bd12885728
habercitr <31370379+ahmtdemir@users.noreply.github.com>
2026-07-18 10:51:05 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+BOT_TOKEN = "8686464914:AAFLYm-zhY5wggUbbouXLKqDIMEtRFr6gFI"
+CHAT_ID = "868647307"
+
+API_URL = "https://dbysmgw.doa.gov.tr/dbys/v3/web/rvm/search?pageNumber=1&pageSize=100"
+
+PAYLOAD = {
+    "lat": 37.21596145629883,
+    "lon": 28.36799430847168,
+    "distance": 2466,
+    "userLat": 37.212301266949034,
+    "userLon": 28.354810181393365
+}
+
+TAKIP_MAKINELERI = [
+    "Q681 BİM NERGİS / MENTEŞE - MUĞLA",
+    "BİM-AYDINLIKEVLER",
+    "MİGROS MUĞLA MM MİGROS"
+]
