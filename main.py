9f29d8fdb0d2d4e02793ab6e6e102dcccb3f0b36
habercitr <31370379+ahmtdemir@users.noreply.github.com>
2026-07-18 09:43:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("DOA Tracker başlatıldı.")
+import os
+import requests
+
+TOKEN = os.environ.get("TELEGRAM_TOKEN")
+CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+payload = {
+    "chat_id": CHAT_ID,
+    "text": "✅ GitHub Actions başarıyla çalışıyor!"
+}
+
+response = requests.post(url, json=payload)
+
+print(response.status_code)
+print(response.text)
