cc8fbf8752e6eb44fa4485fef463c0fb08e22a76
habercitr <31370379+ahmtdemir@users.noreply.github.com>
2026-07-21 10:42:29 +0300

    Remove redundant command formatter patch

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from alarm_level_fix import (
     confirm_boolean_two_way,
     use_alarm_memory,
 )
-from alert_formatter import alert, card, command_card, safe_apply_simultaneous_emptying
+from alert_formatter import alert, card, safe_apply_simultaneous_emptying
 
 TZ = ZoneInfo("Europe/Istanbul")
 OPEN_HOUR = 8
@@ -58,7 +58,6 @@ scraper.confirm_boolean = confirm_boolean_two_way
 scraper.filtered_bin = use_alarm_memory(scraper.filtered_bin)
 scraper.apply_simultaneous_emptying = safe_apply_simultaneous_emptying
 scraper.gecmis_kaydi_ekle = history_with_alarm_record
-commands.machine_card = command_card
 
 current_time = datetime.now(TZ)
 print(f"DOA Tracker başladı: {current_time.strftime('%d.%m.%Y %H:%M')}")
